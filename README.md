e9f8cacf26a4a82a3c4b63b01f9de54b90547a38
Frédéric Fortier <frf@ghgsat.com>
2021-05-11 16:06:18 -0400

    Update setup procedure

--- README.md
+++
@@ -1,4 +0,0 @@
-* `sudo apt-get build-dep openvpn && apt-get source openvpn`
-* apply patch
-* `debchange`
-* `dpkg-buildpackage -rfakeroot -uc -b`
---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -o pipefail
+set -o nounset
+set -o errexit
+set -o errtrace
+
+sudo apt-get build-dep openvpn
+apt-get source openvpn
+cd openvpn-*
+git apply ../*.patch
+debchange
+DEB_BUILD_OPTIONS='parallel=16' debuild -i -us -uc -b
+cd ..
+sudo apt-get install ./openvpn_*_amd64.deb
